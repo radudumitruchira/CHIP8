3e5aefa45dbf345dbbd63edda189cdc23758f26a
Radu Chira <chiraradu2004@gmail.com>
2026-02-13 01:16:50 +0100

    Parsing some command

--- src/main.c
+++ src/main.c
@@ -64,7 +64,7 @@ static uint16_t stack[STACK_SIZE];
 static uint8_t regs[REG_COUNT];
 static uint8_t display[DISPLAY_SIZE];
 static uint8_t keypad[KEY_COUNT];
-static uint16_t reg_pc;
+static uint16_t *reg_pc;
 static uint16_t reg_i;
 
 // static uint8_t keypad[] = {
@@ -145,6 +145,215 @@ static void debug_memory(uint16_t n)
     }
 }
 
+static void instruction_exec(uint32_t size)
+{
+    for (reg_pc = (uint16_t*)(memory + 0x200); reg_pc < (uint16_t *)(memory + 0x200 + size); reg_pc++)
+    {
+        uint8_t msb = (uint8_t)(*reg_pc & 0x00FF);
+        uint8_t lsb = (uint8_t)((*reg_pc & 0xFF00) >> 8);
+        uint16_t instr = (msb << 8) | lsb;
+
+        uint16_t nnn_addr = instr & 0x0FFF;
+        uint8_t nibble = instr & 0x000F;
+        uint8_t x = instr & 0x0F00;
+        uint8_t y = instr & 0x00F0;
+        uint8_t kk_byte = instr & 0x00FF;
+        uint8_t top_nibble = instr & 0xF000;
+
+        switch (top_nibble)
+        {
+            case 0x0: 
+            {
+                switch (instr)
+                {
+                    case 0x00E0:
+                    {
+
+                    } break;
+                    case 0x00EE:
+                    {
+
+                    } break;
+                }
+
+            } break;
+            case 0x1:
+            {
+                reg_pc = (uint16_t *)(memory + nnn_addr);
+            } break;
+            case 0x2:
+            {
+
+            } break;
+            case 0x3:
+            {
+                if (regs[x] == kk_byte)
+                    ++reg_pc;
+                else
+                    continue;
+
+            } break;
+            case 0x4:
+            {
+                if (regs[x] != kk_byte)
+                    ++reg_pc;
+                else
+                    continue;
+            } break;
+            case 0x5:
+            {
+                if (regs[x] == regs[y])
+                    ++reg_pc;
+                else
+                    continue;
+            } break;
+            case 0x6:
+            {
+                regs[x] = kk_byte;
+
+            } break;
+            case 0x7:
+            {
+                regs[x] += kk_byte;
+
+            } break;
+            case 0x8:
+            {
+                switch (nibble)
+                {
+                    case 0x0:
+                    {
+                        regs[x] = regs[y];
+
+                    } break;
+                    case 0x1:
+                    {
+                        regs[x] |= regs[y];
+                    } break;
+                    case 0x2:
+                    {
+                        regs[x] &= regs[y];
+                    } break;
+                    case 0x3:
+                    {
+                        regs[x] ^= regs[y];
+                    } break;
+                    case 0x4:
+                    {
+                        uint8_t v1 = regs[x];
+                        uint8_t v2 = regs[y];
+                        uint16_t result = v1 + v2;
+
+                        if (v1 + v2 > 255)
+                            regs[REG_VF] = 1;
+                        else
+                            regs[REG_VF] = 0;
+
+                        regs[x] = (result & 0x00FF);
+                    } break;
+                    case 0x5:
+                    {
+                        if (regs[x] > regs[y])
+                            regs[REG_VF] = 1;
+                        else
+                            regs[REG_VF] = 0;
+
+                        regs[x] = regs[x] - regs[y];
+                    } break;
+                    case 0x6:
+                    {
+                        if (top_nibble & 0b1000)
+                            regs[REG_VF] = 1;
+                        else
+                            regs[REG_VF] = 0;
+
+                        regs[x] /= 2;
+                    } break;
+                    case 0x7:
+                    {
+                        if (regs[x] > regs[y])
+                            regs[REG_VF] = 1;
+                        else
+                            regs[REG_VF] = 0;
+
+                        regs[x] = regs[y] - regs[x];
+                    } break;
+                    case 0xE:
+                    {
+                        if (nibble & 0b0001)
+                            regs[REG_VF] = 1;
+                        else
+                            regs[REG_VF] = 0;
+
+                        regs[x] *= 2;
+                    } break;
+                } break;
+
+            } break;
+            case 0x9:
+            {
+                if (regs[x] != regs[y])
+                    ++reg_pc;
+                else
+                    continue;
+            } break;
+            case 0xA:
+            {
+                reg_i = nnn_addr;
+            } break;
+            case 0xB:
+            {
+                reg_pc = (uint16_t *)(memory + nnn_addr + regs[0]);
+            } break;
+            case 0xF:
+            {
+                
+                switch (kk_byte)
+                {
+                    case 0x07:
+                    {
+                        regs[x] = regs[REG_DT];
+                    } break;
+                    case 0x0A:
+                    {
+                    } break;
+                    case 0x15:
+                    {
+                        regs[REG_DT] = regs[x];
+                    } break;
+                    case 0x18:
+                    {
+                        regs[REG_ST] = regs[x];
+                    } break;
+                    case 0x1E:
+                    {
+                        reg_i = reg_i + regs[x];
+                    } break;
+                    case 0x29:
+                    {
+
+                    } break;
+                    case 0x33:
+                    {
+                        uint8_t digit = regs[x];
+                        for (int8_t i = 2; i >= 0; i--)
+                        {
+                            uint8_t nr = digit % 10;
+                            *(uint16_t *)(memory + reg_i + i) = nr;
+                            digit /= 10;
+                        }
+
+                    } break;
+                }
+
+            } break;
+        }
+
+        printf("0x%04hX\n", instr);
+    }
+    
+}
+
 static void init()
 {
     memcpy(memory, sprites, sizeof sprites);
@@ -160,12 +369,13 @@ int main(int argc, char const *argv[])
     // }
 
     uint32_t size = 0;
-    uint8_t *program = read_file("../../assets/roms/IBM Logo.ch8", &size);
+    uint8_t *program = read_file("../../../assets/roms/IBM Logo.ch8", &size);
 
     memcpy(&memory[512], program, size);
     free(program);
 
     init();
-    debug_memory((uint16_t)size);
+    //debug_memory((uint16_t)size);
+    instruction_exec(size);
     return 0;
 }
